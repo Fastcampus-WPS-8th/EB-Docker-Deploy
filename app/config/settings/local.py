cabea280ee7d7d1f665e7cdc934d5ce4106f3e92
LeeHanYeong <dev@lhy.kr>
2018-07-13 16:29:59 +0900

    local, production에서 ERROR log파일 생성

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -1,7 +1,9 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+]
 
 WSGI_APPLICATION = 'config.wsgi.local.application'
 
@@ -13,3 +15,44 @@ DATABASES = {
 }
 
 STATIC_URL = '/static/'
+
+LOG_DIR = os.path.join(ROOT_DIR, '.log')
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            'format': '[%(asctime)s] %(message)s',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'file_error': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'ERROR',
+            'formatter': 'django.server',
+            'backupCount': 10,
+            'filename': os.path.join(LOG_DIR, 'error.log'),
+            'maxBytes': 10485760,
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file_error'],
+            'level': 'INFO',
+            'propagate': True,
+        }
+    }
+}
