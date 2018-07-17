0346f1e030fd021e72aa30f807683a88d30b1b6d
LeeHanYeong <dev@lhy.kr>
2018-07-17 14:50:44 +0900

    배포 완료 후 커스텀 셸 스크립트 삭제

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
-# requirements.txt만들기
-pipenv lock --requirements > requirements.txt
-
-# .secrets와 requirements를 staging area에 추가
-git add -f .secrets/ requirements.txt
+# .secrets를 staging area에 추가
+git add -f .secrets/
 
 # eb deploy실행
 eb deploy --profile fc-8th-eb --staged
 
 # .secrets와 requirements를 staging area에서 제거
 git reset HEAD .secrets/ requirements.txt
-
-# requirements.txt삭제
-rm requirements.txt
