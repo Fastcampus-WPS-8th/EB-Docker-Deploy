25b0e033bbb21a42c89833655af14675b618d27f
LeeHanYeong <dev@lhy.kr>
2018-07-17 16:29:19 +0900

    http요청시 https로 redirect설정

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # .secrets를 staging area에 추가
+git add -A
 git add -f .secrets/
 
 # eb deploy실행
@@ -7,3 +8,4 @@ eb deploy --profile fc-8th-eb --staged
 
 # .secrets와 requirements를 staging area에서 제거
 git reset HEAD .secrets/ requirements.txt
+git reset
